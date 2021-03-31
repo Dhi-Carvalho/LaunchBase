0621c87d0b96c31b59ac24fa7a29decff9bab4ef
Dhi-Carvalho <dhi.carvalho85@gmail.com>
2021-03-31 17:31:52 -0300

    Updating classes and challenges

---
+++ Introducao_a_programacao_WEB/Desafio1.3-Funcoes_e_estruturas_de_repeticao/busca_por_tecnologia.js
@@ -0,0 +1,30 @@
+const usuarios = [
+    {
+      nome: "Carlos",
+      tecnologias: ["HTML", "CSS"],
+    },
+    {
+      nome: "Jasmine",
+      tecnologias: ["JavaScript", "CSS"],
+    },
+    {
+      nome: "Tuane",
+      tecnologias: ["HTML", "Node.js"],
+    },
+  ];
+
+  function usuarioUsaCSS(usuario) {
+      for (let tecnologia of usuario.tecnologias) {
+          if (tecnologia == 'CSS')
+          return true;
+      }
+      return false;
+  };
+
+  for (let usuario of usuarios) {
+    const usuarioTrabalhaCSS = usuarioUsaCSS(usuario);
+
+    if(usuarioTrabalhaCSS) {
+        console.log(`O usuário ${usuario.nome} trabalha com CSS.`);
+    }
+  };
